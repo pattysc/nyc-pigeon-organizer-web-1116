53abfc2b1743b1bcaf5e44091a0b4d11decfe4fe
p sc <patty.stacruz@gmail.com>
2016-11-17 23:00:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  newhash = {}
+
+  pigeon_data.values.each do |datahashes|
+    datahashes.values.each do |names|
+      names.each do |name|
+        newhash[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+
+  pigeon_data.each do |category, infohash|
+    infohash.each do |key, names|
+      names.each do |name|
+        newhash[name][category] << key.to_s
+      end
+    end
+  end
+  return newhash
 end
